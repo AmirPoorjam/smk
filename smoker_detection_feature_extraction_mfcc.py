0f93a390ef4226e40036ed3f3206af71efe0040f
Amir <a.h.poorjam@gmail.com>
2019-11-08 11:08:06 +0100

    Comments added, ready to use

--- smoker_detection_feature_extraction_mfcc.py
+++ smoker_detection_feature_extraction_mfcc.py
@@ -1,26 +1,38 @@
+#######################################################################################################################
+# This module extracts 13 MFCC coefficients from all Mixer6 recordings. It reads data from two Mixer6 tabels          #
+# (mx6_calls.csv and mx6_subjs.csv), and make two folders M:male and F:female. In each of the folders it makes two    #
+# subfolders Y: smokers and N: non-smokers. Then, it makes a folder for each speakers and saves features of all       #
+# recordings from each speaker.                                                                                       #
+#                                                                                                                     #
+###### Amir H. Poorjam, November 2019 #################################################################################
+
 import os
 import numpy as np
-import mfcc_extraction as fe
+import acoustic_feature_extraction as fe
 import librosa
 import pandas as pd
 from os import listdir
 
 
-MFCCParam = {'NumFilters': 27, 'NFFT': 512, 'FminHz': 0, 'FMaxHz': 4000, 'no': 12, 'FLT': 0.030, 'FST': 0.020,
-             'vad_flag': 1}
+MFCCParam = {'NumFilters': 27,'NFFT':512,'FminHz':0,'FMaxHz':4000,'no':12,'FLT':0.030,'FST':0.020,'vad_flag':1}
+##### Setting the paths #######
+path_to_tabels = input('Please enter the path to the Mixer6 tables: ')
+sourcefoldedr  = input('Please enter the path to the Mixer6 raw audio files (.sph): ')
+destinationfolder = './features_mfcc/'
+if path_to_tabels[-1] != '/':
+    path_to_tabels = path_to_tabels + '/'
+if sourcefoldedr[-1] != '/':
+    sourcefoldedr = sourcefoldedr + '/'
 
-path_calls = 'C:/Amir/Data/Mixer6/data/docs/mx6_calls.csv'
-path_subjs = 'C:/Amir/Data/Mixer6/data/docs/mx6_subjs.csv'
+##### Reading data from tables #######
+call_id   = np.asarray(pd.read_csv(path_to_tabels + 'mx6_calls.csv', usecols=['call_id']).T.values.tolist()[0])
+spk_a     = np.asarray(pd.read_csv(path_to_tabels + 'mx6_calls.csv', usecols=['sid_a']).T.values.tolist()[0])
+spk_b     = np.asarray(pd.read_csv(path_to_tabels + 'mx6_calls.csv', usecols=['sid_b']).T.values.tolist()[0])
+speakers  = np.asarray(pd.read_csv(path_to_tabels + 'mx6_subjs.csv', usecols=['subjid']).T.values.tolist()[0])
+gender    = np.asarray(pd.read_csv(path_to_tabels + 'mx6_subjs.csv', usecols=['sex']).T.values.tolist()[0])
+smoke_lbl = np.asarray(pd.read_csv(path_to_tabels + 'mx6_subjs.csv', usecols=['smoker']).T.values.tolist()[0])
 
-call_id   = np.asarray(pd.read_csv(path_calls, usecols=['call_id']).T.values.tolist()[0])
-spk_a     = np.asarray(pd.read_csv(path_calls, usecols=['sid_a']).T.values.tolist()[0])
-spk_b     = np.asarray(pd.read_csv(path_calls, usecols=['sid_b']).T.values.tolist()[0])
-speakers  = np.asarray(pd.read_csv(path_subjs, usecols=['subjid']).T.values.tolist()[0])
-gender    = np.asarray(pd.read_csv(path_subjs, usecols=['sex']).T.values.tolist()[0])
-smoke_lbl = np.asarray(pd.read_csv(path_subjs, usecols=['smoker']).T.values.tolist()[0])
-
-sourcefoldedr = 'C:/Amir/Data/Mixer6/data/ulaw_sphere/'
-destinationfolder = 'C:/Amir/Codes/smoker detection/features_mfcc/'
+##### Finding metadata for each recording, extracting features, and saving with the corresponding names #####
 all_files = [f for f in listdir(sourcefoldedr) if f.endswith('.sph')]
 total_file = len(all_files)
 fidx = 0
@@ -53,10 +65,8 @@ for filename in all_files:
         maxamp_a = np.max(abs(signals[0]))
         if maxamp_a != 0:
             norm_sig_a = signals[0] * (1 / maxamp_a)
-            mfcc_features, _, _ = fe.main_mfcc_function(norm_sig_a, fs, MFCCParam)
+            mfcc_features = fe.main_mfcc_function(norm_sig_a, fs, MFCCParam)[0]
             np.save(directory_a + str(id_spk_a[0]) + '_' + filename[0:-4]+'.npy',mfcc_features)
-
-
     if gender_spk_b=='nan' or smoke_spk_b=='nan':
         nmdx += 1
         print('No metadata for speaker %s --> count: %d' % (id_spk_b[0],nmdx))
@@ -67,8 +77,7 @@ for filename in all_files:
         maxamp_b = np.max(abs(signals[1]))
         if maxamp_b != 0:
             norm_sig_b = signals[1] * (1 / maxamp_b)
-            mfcc_features, _, _ = fe.main_mfcc_function(norm_sig_b, fs, MFCCParam)
+            mfcc_features = fe.main_mfcc_function(norm_sig_b, fs, MFCCParam)[0]
             np.save(directory_b + str(id_spk_b[0]) + '_' + filename[0:-4] + '.npy', mfcc_features)
-
     fidx += 1
 
